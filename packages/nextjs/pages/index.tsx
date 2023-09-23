efa98bbecf2a53082065122f8fcac0e60b5799f8
Austin Griffith <austin@ethereum.org>
2023-09-23 14:44:46 -0400

    room address tracking maybe

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,71 +1,62 @@
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { useAccount } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
+  const { address } = useAccount();
+
+  const [accountDisplay, setAccountDisplay] = useState(<div></div>);
+
+  const executeFunction = async () => {
+    console.log("✅ Checked in!");
+    // Your code here
+    const res = fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ address: address }),
+    });
+    const data = await (await res).json();
+    const { allStorage } = data;
+
+    const newAccountDisplay = allStorage?.map((address: any) => {
+      if (address) {
+        return (
+          <div key={address}>
+            <Address address={address} />
+          </div>
+        );
+      }
+    });
+    setAccountDisplay(newAccountDisplay);
+  };
+
+  // yo chat gpt gimme a function that runs at start and every 5 s checking in with my address
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    // Function to be executed on page load and every 5 seconds
+
+    // Execute the function immediately upon page load
+    executeFunction();
+
+    // Set an interval to execute the function every 5 seconds
+    const intervalId = setInterval(executeFunction, 5000);
+
+    // Cleanup: clear the interval when the component is unmounted
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [address]); // The empty dependency array ensures that the effect runs only once when the component mounts
+
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="flex items-center flex-col flex-grow pt-10">{accountDisplay}</div>
     </>
   );
 };
